1ee1a289ae09c97b884439300f011adac1f14ddd
Gage <vadium3854@gmail.com>
2023-05-10 23:35:10 -0400

    Created 16 divs

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+}
+
+for (let j = 0; j < 16; j++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    row.appendChild(square);
+}
+
+
