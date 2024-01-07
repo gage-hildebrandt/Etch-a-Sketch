622c9f3c6898837bb3ad01fed43e2f362d71f99f
Gage <vadium3854@gmail.com>
2024-01-07 03:48:19 -0500

    added grid background color and eventlisteners for button size

--- javascript.js
+++ javascript.js
@@ -1,18 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
+    createBoard(32);
 
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
 function createBoard(size) {
     let board = document.querySelector('.board');
 
-    board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-    board.style.gridTemplateRows = 'repeat(16, 1fr)';
+    board.style.gridTemplateColumns = `repeat(${size},(16, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}(16, 1fr)`;
 
     let numDivs = size * size;
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement('div');
-        board.insertAdjacentElementBeforeElement('beforeend, div', div);
+        div.style.backgroundColor = ('yellow');
+        board.insertAdjacentElement('beforeend', div);
     }
     
+}
+
+function getSize(){
+    let input = prompt('What will be the size of the board?')
+    let message = document.querySelector('#message');
+    if(input == ''){
+        message.innerHTML = 'Please provide a number';
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = 'Please provide a number between 1 and 100'
+    }
+    else{
+        message.innerHMTL = 'Have fun playing!'
+    }
 }
