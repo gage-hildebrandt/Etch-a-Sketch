5a97fe6968216e6f5e5d41edff1a5eae5c36d3f2
Gage <vadium3854@gmail.com>
2024-01-07 03:15:07 -0500

    Made board grid function

--- javascript.js
+++ javascript.js
@@ -1,22 +1,18 @@
-const container = document.getElementById('grid-container');
+document.addEventListener("DOMContentLoaded", function() {
 
-// Function to create the grid
-function createGrid(rows, cols) {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const gridItem = document.createElement('div');
-            gridItem.classList.add('grid-item');
-            container.appendChild(gridItem); // Append each grid item to the container
-        }
+})
+
+function createBoard(size) {
+    let board = document.querySelector('.board');
+
+    board.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    board.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement('div');
+        board.insertAdjacentElementBeforeElement('beforeend, div', div);
     }
+    
 }
-
-// Call the createGrid function to create a 16x16 grid
-createGrid(16, 16);
-
-
-// Add border to the grid container
-container.style.border = '1px solid black';
-container.style.backgroundColor = 'red';
-
-
