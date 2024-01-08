43dd35c1964eda4503ca6708c059f8c577a7cc1f
Gage <vadium3854@gmail.com>
2024-01-07 22:05:55 -0500

    added click function for drawing, and finished core code for the app

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
 let color = 'black';
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
+
+    document.querySelector('body').addEventListener('click', function(e){
+        if(e.target.tagName != 'BUTTON'){
+            click = !click;
+            let draw = document.querySelector('#draw');
+            if(click){
+                draw.innerHTML = 'You can draw';
+            }
+            else{
+                draw.innerHTML = 'You cannot draw';
+            }
+        }
+    });
 
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener('click', function(){
@@ -42,13 +56,20 @@ function getSize(){
 }
 
 function colorDiv(){
-    if(color == 'random'){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else{
+    if(click){
+        if(color == 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
         this.style.backgroundColor = 'black';
+        }
     }
 }
 function setColor(colorChoice){
     color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll('div');
+    divs.forEach((div)=> div.style.backgroundColor = 'white');
 }
